c01028c821c1b0fb5277fc73c1c0a6454f1bf637
Roger Generoso Masós <roger21gm@gmail.com>
2019-12-23 16:06:36 +0100

    Implemented same business api logic using a Hook.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,32 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
 
-    const searchApi = async (searchTerm) => {
-        try{
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'Barcelona',
-                }
-            });
-            setResults(response.data.businesses);
-            setErrorMessage('');
-        } catch (error) {
-            setErrorMessage('Something went wrong');
-        }
-    };
-
-    useEffect( () => {
-        searchApi('');
-    }, [])
+    const [searchApi, results, errorMessage] = useResults();
 
     return (
         <View>
